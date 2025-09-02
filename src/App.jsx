2b787b240c751bf8eeb02ceed467ddee5e360804
MohamedATeama <mohammed.teama789@gmail.com>
2025-09-02 23:23:16 +0300

    handle deploy

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/WorldWise">
           <Suspense fallback={<SpinnerFullPage />}>
             <Routes>
               <Route index element={<Homepage />} />
